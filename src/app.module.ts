20c71e04b42e2768ef1ee53e666912dc59db8960
philipimperato <philipimperato@gmail.com>
2023-09-12 17:10:53 -0400

    Fix module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './app/users/users.module';
 import sequelizeConfig from './sequelize.config';
-import { DefaultQueryPagination } from './middleware/default-query-pagination';
 import { AuthModule } from './app/auth/auth.module';
 import { AuthController } from './app/auth/auth.controller';
 
@@ -14,22 +12,6 @@ import { AuthController } from './app/auth/auth.controller';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [sequelizeConfig],
-    }),
-    SequelizeModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async () => {
-        return {
-          dialect: 'postgres',
-          host: 'localhost',
-          port: 5433,
-          username: 'postgres',
-          password: 'e2e@testdb',
-          database: 'postgres',
-          autoLoadModels: true,
-          synchronize: true,
-        };
-      },
-      inject: [ConfigService],
     }),
     UsersModule,
     AuthModule,
